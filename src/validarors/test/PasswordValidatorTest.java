9f3c0392b9c8c330513f5d0fb4ea2b4b951cb6d2
SergTestcom <laffaet0309@gmail.com>
2025-06-20 11:10:27 +0200

    added tests: testValidPassword, testSymbolsLessThan8, testInvalidSpecSymbol, testNoUpperCase testNoLowerCase testNull

--- src/validarors/test/PasswordValidatorTest.java
+++ src/validarors/test/PasswordValidatorTest.java
@@ -1,4 +1,59 @@
 package validarors.test;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import validarors.utils.PasswordValidator;
+
+import javax.annotation.processing.SupportedAnnotationTypes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PasswordValidatorTest {
+    static PasswordValidator validarot;
+
+    @BeforeAll
+    static void setUpAll(){
+        validarot = new PasswordValidator();
+
+    }
+
+    @Test
+    void testValidPassword(){
+        assertTrue(PasswordValidator.isStrong("Qwertty0!"));
+        assertTrue(PasswordValidator.isStrong("Qwertty0?"));
+        assertTrue(PasswordValidator.isStrong("Qwertty0@"));
+        assertTrue(PasswordValidator.isStrong("Qwertty0#"));
+    }
+
+    @Test
+    void testSymbolsLessThan8(){
+        //TODO
+
+    }
+
+    @Test
+    void testInvalidSpecSymbol(){
+        assertFalse(PasswordValidator.isStrong("Qwerty0 "));
+        assertFalse(PasswordValidator.isStrong("Qwe^rty0"));
+        assertFalse(PasswordValidator.isStrong("~Qwerty0"));
+
+    }
+
+    @Test
+    void testNoUpperCase(){
+        assertFalse(PasswordValidator.isStrong("qwerty2!"));
+    }
+
+    @Test
+    void testNoLowerCase(){
+        assertFalse(PasswordValidator.isStrong("QWE§RTY2!"));
+
+    }
+
+    @Test
+    void testNull() {
+         assertFalse(PasswordValidator.isStrong(null));
+
+    }
+
 }
